1b6f77886c6890561e98997eb80883994d877387
Abdul Hamid HmDi <axmxh@hotmail.com>
2020-02-04 04:13:33 +0200

    Updates

---
+++ precache-manifest.fae89d722cc69cb3bf134c023665b820.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e225ad957c2c69095d45e871d14f616a",
+    "url": "/genres/index.html"
+  },
+  {
+    "revision": "eaafd566d03b90082bd7",
+    "url": "/genres/static/js/2.23fde228.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/genres/static/js/2.23fde228.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3625e35f85d035ea30a5",
+    "url": "/genres/static/js/main.df51fbab.chunk.js"
+  },
+  {
+    "revision": "cdccaeb47de2117d06ab",
+    "url": "/genres/static/js/runtime-main.e4119525.js"
+  }
+]);
