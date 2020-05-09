f389f7807768a4d73c84938db253b06c42864bff
Abdul Hamid HmDi <abdelhamid@uxbert.com>
2020-05-10 00:44:13 +0200

    fix cors

--- src/apis/deezer.js
+++ src/apis/deezer.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from "axios";
 /**
  *  if you run the project locally you have to use commented baseURL:
  *  to bypass CORS policy
@@ -6,6 +6,6 @@ import axios from 'axios';
  */
 
 export default axios.create({
-  // baseURL: 'https://cors-anywhere.herokuapp.com/https://api.deezer.com'
-  baseURL: 'https://api.deezer.com'
+  baseURL: "https://cors-anywhere.herokuapp.com/https://api.deezer.com"
+  //baseURL: 'https://api.deezer.com'
 });
